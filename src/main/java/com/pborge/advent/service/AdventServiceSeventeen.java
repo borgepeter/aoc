6b5aa525584c7fead2b7f8724e28fd6bfb655c7b
Peter Borge <borgepeter@gmail.com>
2020-12-20 21:37:00 -0500

    Day 17

--- src/main/java/com/pborge/advent/service/AdventServiceSeventeen.java
+++ src/main/java/com/pborge/advent/service/AdventServiceSeventeen.java
@@ -3,9 +3,12 @@ package com.pborge.advent.service;
 import com.pborge.advent.models.Cell;
 import com.pborge.advent.models.Cube;
 import com.pborge.advent.models.Floor;
+import com.pborge.advent.models.HyperCube;
 import com.pborge.advent.repository.WebRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class AdventServiceSeventeen {
@@ -31,18 +34,43 @@ public class AdventServiceSeventeen {
     }
 
     public Integer getAdvent2Result() {
-        return null;
+        HyperCube hyperCube = createHyperCube();
+        hyperCube.print();
+
+        for (int i = 0; i < BOOT_CYCLES; i++) {
+            hyperCube = step2(hyperCube, 3, 2, 3);
+            hyperCube.print();
+        }
+
+        return hyperCube.getOccupiedSeats();
+    }
+
+    private HyperCube createHyperCube() {
+        String[] rawData = webRepository.getData(17).split("\n");
+        int width = rawData.length + BOOT_CYCLES * 2;
+        int length = rawData[0].split("").length + BOOT_CYCLES * 2;
+        HyperCube hyperCube = new HyperCube(width, length, BOOT_CYCLES + 1, BOOT_CYCLES + 1);
+
+        for (int x = 0; x < rawData.length; x++) {
+            for (int y = 0; y < rawData[x].split("").length; y++) {
+                String cellData = rawData[x].split("")[y].replace(".", "L");
+                hyperCube.add(new Cell(cellData), x + BOOT_CYCLES, y + BOOT_CYCLES, 0, 0);
+            }
+        }
+
+        return hyperCube;
     }
 
     private Cube createCube() {
         String[] rawData = webRepository.getData(17).split("\n");
-        Cube cube = new Cube(rawData.length, rawData[0].split("").length, BOOT_CYCLES + 1);
-        for (int z = 0; z <= BOOT_CYCLES; z++) {
-            for (int x = 0; x < cube.getFloor(0).getWidth(); x++) {
-                for (int y = 0; y < cube.getFloor(0).getHeight(); y++) {
-                    String cellData = z == 0 ? rawData[x].split("")[y].replace(".", "L") : "L";
-                    cube.add(new Cell(cellData), x, y, z);
-                }
+        int width = rawData.length + BOOT_CYCLES * 2;
+        int length = rawData[0].split("").length + BOOT_CYCLES * 2;
+        Cube cube = new Cube(width, length, BOOT_CYCLES + 1);
+
+        for (int x = 0; x < rawData.length; x++) {
+            for (int y = 0; y < rawData[x].split("").length; y++) {
+                String cellData = rawData[x].split("")[y].replace(".", "L");
+                cube.add(new Cell(cellData), x + BOOT_CYCLES, y + BOOT_CYCLES, 0);
             }
         }
 
@@ -50,31 +78,55 @@ public class AdventServiceSeventeen {
     }
 
     private Cube step(Cube cube, int spontaneousLife, int minOvercrowdDeath, int maxOvercrowdDeath) {
-        Cube nextCube = new Cube(cube.getWidth() + 2, cube.getHeight() + 2, cube.getDepth());
+        Cube nextCube = new Cube(cube.getWidth(), cube.getHeight(), cube.getDepth());
         for (int z = 0; z < cube.getDepth(); z++) {
             Floor nextFloor = new Floor(nextCube.getWidth(), nextCube.getHeight());
             for (int x = 0; x < nextCube.getWidth(); x++) {
                 for (int y = 0; y < nextCube.getHeight(); y++) {
                     int occupiedNeighbors = cube.countOccupiedNeighbors(x, y, z);
-                    boolean isOccupied = x < cube.getWidth() && y < cube.getHeight() && cube.getFloor(z).get(x, y).isOccupied();
+                    boolean isOccupied = cube.getFloor(z).get(x, y).isOccupied();
                     if (isOccupied && (occupiedNeighbors < minOvercrowdDeath || occupiedNeighbors > maxOvercrowdDeath)) {
                         isOccupied = false;
-                        //System.out.println("Cell ["+x+", "+y+", "+z+"] becomes empty with "+occupiedNeighbors+" neighbors");
                     }
                     else if (!isOccupied && occupiedNeighbors == spontaneousLife) {
                         isOccupied = true;
-                        //System.out.println("Cell ["+x+", "+y+", "+z+"] comes to life with "+occupiedNeighbors+" neighbors");
-                    } else if (isOccupied) {
-                        //System.out.println("Cell ["+x+", "+y+", "+z+"] remains occupied with "+occupiedNeighbors+" neighbors");
                     }
 
                     if (isOccupied)
-                        nextFloor.add(new Cell("#"), x + 1, y + 1);
+                        nextFloor.add(new Cell("#"), x, y);
                 }
             }
             nextCube.add(nextFloor, z);
         }
 
         return nextCube;
+    }
+
+    private HyperCube step2(HyperCube hyperCube, int spontaneousLife, int minOvercrowdDeath, int maxOvercrowdDeath) {
+        HyperCube nextHyperCube = new HyperCube(hyperCube.getWidth(), hyperCube.getHeight(), hyperCube.getDepth(), hyperCube.getTime());
+        for (int n = 0; n < hyperCube.getTime(); n++) {
+            Cube nextCube = new Cube(hyperCube.getWidth(), hyperCube.getHeight(), hyperCube.getDepth());
+            for (int z = 0; z < hyperCube.getDepth(); z++) {
+                Floor nextFloor = new Floor(hyperCube.getWidth(), hyperCube.getHeight());
+                for (int x = 0; x < hyperCube.getWidth(); x++) {
+                    for (int y = 0; y < hyperCube.getHeight(); y++) {
+                        int occupiedNeighbors = hyperCube.countOccupiedNeighbors(x, y, z, n);
+                        boolean isOccupied = hyperCube.getCube(n).getFloor(z).get(x, y).isOccupied();
+                        if (isOccupied && (occupiedNeighbors < minOvercrowdDeath || occupiedNeighbors > maxOvercrowdDeath)) {
+                            isOccupied = false;
+                        } else if (!isOccupied && occupiedNeighbors == spontaneousLife) {
+                            isOccupied = true;
+                        }
+
+                        if (isOccupied)
+                            nextFloor.add(new Cell("#"), x, y);
+                    }
+                }
+                nextCube.add(nextFloor, z);
+            }
+            nextHyperCube.add(nextCube, n);
+        }
+
+        return nextHyperCube;
     }
 }
